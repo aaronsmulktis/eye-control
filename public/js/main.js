03daacc315a1f933be5b3e8783ff9f5eb8690aa4
Benjamin Gleitzman <gleitz@mit.edu>
2015-11-20 08:26:07 -0800

    Change names to match existing locations

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,8 @@
 /*global google*/
 
 var homes = [
-  ['40 Warwick Ave.', 51.523325, -0.183299, '/home1'],
-  ['Buckingham Palace', 51.501483, -0.141948, '/home1'],
+  ['Elysten', 51.523325, -0.183299, '/home1'],
+  ['Gateways', 51.501483, -0.141948, '/home1'],
   ['Ministry of Sound', 51.497740, -0.099440, '/home1'],
   ['22 Hugon Road', 51.467892, -0.191422, '/home1'],
   ['Radcliff House', 51.490478, -0.061075, '/home1']
