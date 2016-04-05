b31d11be80b4f01a80162975811bbffa0bbe1be9
Markitos1024 <mjacue@itcrowdarg.com>
2016-04-05 16:11:23 -0300

    add cookie service

--- map.jsx
+++ map.jsx
@@ -56,6 +56,9 @@ Map = React.createClass({
     },
 
     initialize() {
+        
+        console.log("Id's vistos:");
+        console.log(Cookie.getViewed());
        
         if (this.getBg() != "ffffff") {
             styles.push({stylers: [{ hue: "#"+this.getBg() } ] });
