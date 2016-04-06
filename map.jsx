93c6c4e53880bbec5871f008b404af6b7871c207
Markitos1024 <mjacue@itcrowdarg.com>
2016-04-06 14:02:26 -0300

    minor UI changes for phase 1

--- map.jsx
+++ map.jsx
@@ -113,11 +113,12 @@ Map = React.createClass({
 
     createMarker(lat, lon, html, link, position,i,id) {
        let state = this.state;
+       let colorViewed = '663366';
         //create a marker
         let newMarker = new google.maps.Marker({
                 position: new google.maps.LatLng(lat, lon),
                 map: state.mainMap,
-                icon: {url: "/img/"+( ((typeof vistas === 'object') && (vistas.indexOf(id)>=0)) ? '000000': this.getBg())+".png"},
+                icon: {url: "/img/"+( ((typeof vistas === 'object') && (vistas.indexOf(id)>=0)) ? colorViewed : this.getBg())+".png"},
                 label: i.toString(),
                 path: google.maps.SymbolPath.CIRCLE,
                 title: '',
@@ -154,15 +155,16 @@ Map = React.createClass({
         }
         for (let i = 0; i < homes.length; i++) {
             let homeName = homes[i].name,
-                homeDesc = homes[i].desc,
+                homeDesc = homes[i].desc ? homes[i].desc : "",
                 homePrice = homes[i].price,
                 homeRooms = homes[i].numBedrooms,
                 homeBaths = homes[i].numBathrooms,
                 homeThumb = homes[i].propPic;
+                homeLink = 'home/' + homes[i].name+"/"+homes[i]._id;
                 homePrice = homePrice && accounting.formatMoney(homePrice, "£", 0, ".", ",");
                 let homeThumbUrl = homeThumb && 'http://vault.ruselaboratories.com/proxy?url=' + encodeURIComponent(homeThumb) + '&resize=1&width=200',
-                imageDiv =  "<div class='col-sm-4 noPadding pull-left'> <img data-url='"+ homeThumb +"' src='"+ homeThumbUrl +"'></img> </div>" ,
-                descriptionDiv = "<div class=' col-sm-8 pull-right'> <h3 class='no-margin'>" + homeName + "</h3><p class='no-margin'>" + homeDesc + "</p> <p class='no-margin'>" + homeRooms + " <i class='fa fa-bed'></i> | " + homeBaths + " <i class='fa fa-recycle'></i></p> <h6 class='no-margin'>" + homePrice + "</h6></div>",
+                imageDiv =  "<a class='mapPopup' href='"+homeLink+"'><div class='col-sm-4 noPadding pull-left'> <img data-url='"+ homeThumb +"' src='"+ homeThumbUrl +"'></img> </div>" ,
+                descriptionDiv = "<div class=' col-sm-8 pull-right'> <h3 class='no-margin'>" + homeName + "</h3><p class='no-margin'>" + homeDesc + "</p> <p class='no-margin'>" + homeRooms + " <i class='fa fa-bed'></i> | " + homeBaths + " <i class='fa fa-recycle'></i></p> <h6 class='no-margin'>" + homePrice + "</h6></div></a>",
                 content = imageDiv + descriptionDiv;
             this.createMarker(homes[i].latitude, homes[i].longitude, content, 'home/' + homes[i]._id, homes[i].position,(i+1),homes[i]._id);
         }
