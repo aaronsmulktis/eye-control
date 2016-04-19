e38646abf9d06c6f4b752abeb04d96391c91333b
jhs458 <jhs458@gmail.com>
2016-04-19 09:24:18 -0700

    removed test in header

--- header.jsx
+++ header.jsx
@@ -187,7 +187,7 @@ Header = React.createClass({
 
             <div className="collapse navbar-collapse">
               <ul className="navbar-nav navbar-right">
-                <li><a href="/"><i className="fa fa-navicon"></i>&nbsp;Listings test</a></li>
+                <li><a href="/"><i className="fa fa-navicon"></i>&nbsp;Listings</a></li>
                 <li><a href="javascript:;" onClick={this._togglePopup}><i className="fa fa-plus-circle"></i>&nbsp;Add Property</a></li>
                 <li><a href="/about"><i className="fa fa-info-circle"></i>&nbsp;About</a></li>
                 <li><a href="javascript:;"><i className="fa fa-home" />&nbsp;Thompson Family</a></li>
